3dc2ed3de639b34af4291da58bfe08b0e444389b
br284 <br284@4f8c4b94-ce0d-0410-8a93-94716ef3bdeb>
2007-03-27 03:39:33 +0000

    iPod Exporter updates
    git-svn-id: https://books-macosx.svn.sourceforge.net/svnroot/books-macosx/Books@83 4f8c4b94-ce0d-0410-8a93-94716ef3bdeb

--- iPod Exporter.app/Contents/Resources/script.sh
+++ iPod Exporter.app/Contents/Resources/script.sh
@@ -2,4 +2,4 @@
 
 JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.5/Home
 
-$JAVA_HOME/bin/java -jar ipod.jar /tmp/books-export/books-export.xml "$1"
+$JAVA_HOME/bin/java -jar ipod.jar /tmp/books-export/books-export.xml "$1" > /tmp/ipod-export.log
