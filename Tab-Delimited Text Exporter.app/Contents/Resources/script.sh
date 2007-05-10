20d83f5ed2dadf66bdf3bb7c24b47cbbef27d77a
br284 <br284@4f8c4b94-ce0d-0410-8a93-94716ef3bdeb>
2007-05-10 14:00:20 +0000

    git-svn-id: https://books-macosx.svn.sourceforge.net/svnroot/books-macosx/Books@99 4f8c4b94-ce0d-0410-8a93-94716ef3bdeb

--- Tab-Delimited Text Exporter.app/Contents/Resources/script.sh
+++ Tab-Delimited Text Exporter.app/Contents/Resources/script.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-/usr/bin/xsltproc tab-delimited.xsl /tmp/books-export/books-export.xml > "$1"
+# /usr/bin/xsltproc tab-delimited.xsl /tmp/books-export/books-export.xml > "$1"
+
+java -jar tab-delimited.jar $1
