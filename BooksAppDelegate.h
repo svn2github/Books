71d7c6059bb0d91eaa84ed000c4880d37f25ee17
br284 <br284@4f8c4b94-ce0d-0410-8a93-94716ef3bdeb>
2008-03-29 01:13:10 +0000

    git-svn-id: https://books-macosx.svn.sourceforge.net/svnroot/books-macosx/Books@231 4f8c4b94-ce0d-0410-8a93-94716ef3bdeb

--- BooksAppDelegate.h
+++ BooksAppDelegate.h
@@ -191,7 +191,7 @@
 
 - (void) endProgressWindow;
 - (void) startProgressWindow: (NSString *) message;
-- (void) refreshComboBoxes:(NSTimer *) theTimer;
+// - (void) refreshComboBoxes:(NSTimer *) theTimer;
 
 - (BOOL)application:(NSApplication *)sender delegateHandlesKey:(NSString *)key;
 
